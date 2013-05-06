342a5cd97f1eb6218df29810afd3d14cdc433511
bigthyme <asinghavong@gmail.com>
2013-05-06 16:27:20 -0700

    Fixed record.js and added bootstrap

--- src/record.js
+++ src/record.js
@@ -1,6 +1,7 @@
 //record.js: the functionality behind the record button
 var recordVideo = function(){
   navigator.getUserMedia({video: true, audio: true}, function(stream){
+      //v is the video element set in main.js
       v.src = window.URL.createObjectURL(stream);
     });
 }
