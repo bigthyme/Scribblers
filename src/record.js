7581ea7cc69e91ea95bc0064e0852d4028efa8ec
c <chadreed.intl@gmail.com>
2013-05-14 17:02:09 -0700

    added instructions in modal

--- src/record.js
+++ src/record.js
@@ -3,7 +3,7 @@
 /*global localStream:false, v:false, localStream:false */
 
 var recordVideo = function(){
-  navigator.getUserMedia({video: true, audio: true},  function(stream){
+  navigator.getUserMedia({video: true},  function(stream){
       localStream = stream;
       //v is the video element set in main.js
       v.src = window.URL.createObjectURL(localStream);
