a4e7c0e0a79d4a42e170942c25902d0bdc6ed6c3
bigthyme <asinghavong@gmail.com>
2015-02-11 20:38:41 -0800

    fix for error callback in getUserMedia

--- src/record.js
+++ src/record.js
@@ -3,14 +3,20 @@
 /*global localStream:false, v:false, localStream:false */
 
 var recordVideo = function(){
-  navigator.getUserMedia({video: true},  function(stream){
-    localStream = stream;
-    //v is the video element set in main.js
-    v.src = window.URL.createObjectURL(localStream);
-  	if($('video').attr('src')){
-  		displayElement = 'video';
-  	  $('.arrow').remove();
-  	};
-  });
+  navigator.getUserMedia({video: true}, success, error);
   videoRunning = true;
+};
+
+var success = function(stream){
+  localStream = stream;
+  //v is the video element set in main.js
+  v.src = window.URL.createObjectURL(localStream);
+  if($('video').attr('src')){
+    displayElement = 'video';
+    $('.arrow').remove();
+  };
+};
+
+var error = function(err) {
+  console.log("The following error occured: " + err);
 };
